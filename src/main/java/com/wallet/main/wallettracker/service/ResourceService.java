4ebc1533dc75403aba3f24a1b6f169cddc3281c2
Wakestand <wakeisle9933@gmail.com>
2024-04-05 09:18:51 +0900

    add resource control class

---
+++ src/main/java/com/wallet/main/wallettracker/service/ResourceService.java
@@ -0,0 +1,10 @@
+package com.wallet.main.wallettracker.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ResourceService {
+
+}
