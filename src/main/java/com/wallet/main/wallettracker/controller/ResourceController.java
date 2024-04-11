5d1a3077a02db108368c7b0274aad5d4a279d5b5
wakeisle9933 <wakeisle9933@gmail.com>
2024-04-11 23:02:30 +0900

    add Resource control logic

--- src/main/java/com/wallet/main/wallettracker/controller/ResourceController.java
+++ src/main/java/com/wallet/main/wallettracker/controller/ResourceController.java
@@ -1,9 +1,14 @@
 package com.wallet.main.wallettracker.controller;
 
+import com.wallet.main.wallettracker.service.ResourceService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ResourceController {
+
+  private final ResourceService resourceService;
 
 
 }
--- src/main/java/com/wallet/main/wallettracker/service/ResourceService.java
+++ src/main/java/com/wallet/main/wallettracker/service/ResourceService.java
@@ -1,10 +1,18 @@
 package com.wallet.main.wallettracker.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ResourceService {
+
+  @Value("${app.emails.file.path}")
+  private String emailsFilePath;
+
+  @Value("${app.base.file.path}")
+  private String baseAddressPath;
+
 
 }
